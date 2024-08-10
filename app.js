0ac1fbc76d9d48d76efa4ee403e3e419d6bccbf2
MVCSystems <carlosmerav@gmail.com>
2024-08-10 01:00:12 -0500

    Add functions

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-
-// console.log('Hello World!');
-
-// Esta es mi función que suma dos números
-const sum = (a,b) => {
-    return a + b
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
 }
 
-// Solo un registro en consola para nosotros
-// console.log(sum(7,3))
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a yenes
+    let valueInYen = valueInDollar * 146.67;
+    // Retornamos el valor en yenes
+    return valueInYen;
+}
 
-// Exporta la función para usarla en otros archivos 
-// (similar a la palabra clave "export" cuando se usa webpack)
-module.exports = {sum};
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a libras
+    let valueInPound = valueInYen * 0.0054;
+    // Retornamos el valor en libras
+    return valueInPound;
+}
+
+
+// Exportamos la función para usarla en otros archivo
+module.exports = { fromEuroToDollar , fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -1,12 +1,60 @@
-// Importar la función sum del archivo app.js
-const {sum}  = require('./app');
-// Comienza tu primera prueba
-test( 'adds 14 + 9 to equal 23', () => {
-    // Dentro de la prueba llamamos a nuestra función sum con 14 y 9
-    // y esperamos que retorne 23
-    expect(sum(14, 9)).toBe(23);
-});
+const { LuTestTube2 } = require('react-icons/lu');
 
-//Node Js
-/* Function expect*/
-/* Function test*/
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+let oneDollarIs = {
+    "JPY": 146.67, // japan yen
+    "EUR": 0.93, // euro
+    "GBP": 0.79, // british pound
+}
+
+let oneYenIs = {
+    "USD": 0.0068, // us dollar
+    "EUR": 0.0062, // euro
+    "GBP": 0.0054, // british pound
+}
+
+
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("One dolar equivale a 146.67 yen", function() {
+
+    // Importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    const yen = fromDollarToYen(10);
+    const expected = 10 * 146.67;
+
+    // Hago mi comparación (la prueba)
+    expect(fromDollarToYen(10)).toBeCloseTo(1466.67);
+})
+
+
+test("One yen equivale a 0.0054 pound", function() {
+    // Importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const pound = fromYenToPound(10);
+    const expected = 10 * 0.0054;
+
+    // Hago mi comparación (la prueba)
+    expect(fromYenToPound(10)).toBeCloseTo(0.054);
+})
